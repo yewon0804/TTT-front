79a23d326152e0d0d66b2d2a6d14b82828be4187
yewon0804 <ehdnjs0804@gmail.com>
2023-07-03 01:59:21 +0900

    design: 마이페이지 레이아웃을 상단부/하단부로 분리 + 하단부 메뉴 추가 (#12)

--- lib/screens/mypage.dart
+++ lib/screens/mypage.dart
@@ -26,197 +26,240 @@ class _MyPageState extends State<MyPage> {
             color: Color(0xffebebeb),
             padding: EdgeInsets.fromLTRB(0, 4, 0, 4),
             child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween, // 추후 메뉴 추가로 인해 스크롤 메뉴가 될 시, 해당 속성은 불필요 할 듯
               children: [
-                Container(
-                  color: Colors.white,
-                  padding: EdgeInsets.all(10),
-                  // decoration: BoxDecoration(
-                  //     color: Colors.white,
-                  //   border: Border.all(
-                  //     color: Colors.white,
-                  //   ),
-                  //   borderRadius: BorderRadius.all(Radius.circular(4))
-                  // ),
-                  child: Row( // 프로필 사진, 프로필 정보(닉네임)
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Row(
+                Column( // 상단
+                  children: [
+                    Container( // 프로필 사진, 프로필 정보(닉네임)
+                      color: Colors.white,
+                      padding: EdgeInsets.all(10),
+                      // decoration: BoxDecoration(
+                      //     color: Colors.white,
+                      //   border: Border.all(
+                      //     color: Colors.white,
+                      //   ),
+                      //   borderRadius: BorderRadius.all(Radius.circular(4))
+                      // ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
-                          InkWell(
-                            onTap: () {
-                              print("클릭 시, 프로필 이미지 변경으로 이동");
-                            },
-                            child: Container(
-                                width: 60,
-                                height: 60,
-                                decoration: BoxDecoration(
-                                    shape: BoxShape.circle,
-                                    image: DecorationImage(
-                                        image: AssetImage('assets/images/mockimg1.jpg'),
-                                        fit: BoxFit.cover
-                                    )
-                                )
-                            ),
-                          ),
-                          Container(
-                            margin: EdgeInsets.fromLTRB(16, 4, 16, 4),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  "티티티닉네임",
-                                  textAlign: TextAlign.start,
-                                ),
-                                Text(
-                                  "email@email.com",
-                                  textAlign: TextAlign.start,
-                                ),
-                              ],
-                            ),
-                          ),
-                        ],
-                      ),
-                      Container(
-                        child: TextButton(
-                          onPressed: () { 
-                            print("내 정보 페이지로 이동");
-                          },
-                          child: Row(
+                          Row(
                             children: [
-                              Text(
-                                "내 정보 ",
-                                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                                textAlign: TextAlign.end,
+                              InkWell(
+                                onTap: () {
+                                  print("클릭 시, 프로필 이미지 변경으로 이동");
+                                },
+                                child: Container(
+                                    width: 60,
+                                    height: 60,
+                                    decoration: BoxDecoration(
+                                        shape: BoxShape.circle,
+                                        image: DecorationImage(
+                                            image: AssetImage('assets/images/mockimg1.jpg'),
+                                            fit: BoxFit.cover
+                                        )
+                                    )
+                                ),
                               ),
-                              Icon(Icons.arrow_forward_ios_sharp, size: 16,)
+                              Container(
+                                margin: EdgeInsets.fromLTRB(16, 4, 16, 4),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      "티티티닉네임",
+                                      textAlign: TextAlign.start,
+                                    ),
+                                    Text(
+                                      "email@email.com",
+                                      textAlign: TextAlign.start,
+                                    ),
+                                  ],
+                                ),
+                              ),
                             ],
                           ),
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-                Column( // 안내: 연말정산, 공지사항, 자주하는 질문, 1:1 문의
-                  children: [
-                    Container( // 타이틀
-                      margin: EdgeInsets.fromLTRB(16, 8, 16, 2),
-                      alignment: Alignment.bottomLeft,
-                      child: Text(
-                        "안내",
+                          Container(
+                            child: TextButton(
+                              onPressed: () {
+                                print("내 정보 페이지로 이동");
+                              },
+                              child: Row(
+                                children: [
+                                  Text(
+                                    "내 정보 ",
+                                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                                    textAlign: TextAlign.end,
+                                  ),
+                                  Icon(Icons.arrow_forward_ios_sharp, size: 16,)
+                                ],
+                              ),
+                            ),
+                          )
+                        ],
                       ),
                     ),
-                    Container( // 메뉴들 수납 공간
-                      color: Colors.white,
-                      padding: EdgeInsets.fromLTRB(16, 4, 16, 4),
-                      child: Row(
+                    Column( // 안내: 연말정산, 공지사항, 자주하는 질문, 1:1 문의
+                      children: [
+                        Container( // 타이틀
+                          margin: EdgeInsets.fromLTRB(16, 8, 16, 2),
+                          alignment: Alignment.bottomLeft,
+                          child: Text(
+                            "안내",
+                          ),
+                        ),
+                        Container( // 메뉴들 수납 공간
+                          color: Colors.white,
+                          padding: EdgeInsets.fromLTRB(16, 4, 16, 4),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: [
+                              Container( // 메뉴: 안내 > 연말정산
+                                child: InkWell(
+                                  onTap: () {
+                                    print("연말정산 페이지로 이동");
+                                  },
+                                  child: Column(
+                                    children: [
+                                      Container(
+                                          margin: EdgeInsets.all(6),
+                                          child: Icon(
+                                            Icons.airplane_ticket_sharp,
+                                            color: Colors.grey,
+                                          )
+                                      ),
+                                      Text(
+                                        "연말정산",
+                                        style: TextStyle(
+                                            fontSize: 12,
+                                            color: Colors.grey // -> 연말정산 업데이트 되었을 때는, ttt 테마 색상으로 깜빡이게 하면 어떨까?
+                                        ),
+                                      )
+                                    ],
+                                  ),
+                                ),
+                              ),
+                              Container( // 메뉴: 안내 > 공지사항
+                                child: InkWell(
+                                  onTap: () {
+                                    print("공지사항 페이지로 이동");
+                                  },
+                                  child: Column(
+                                    children: [
+                                      Container(
+                                          margin: EdgeInsets.all(6),
+                                          child: Icon(
+                                            Icons.announcement_sharp,
+                                            color: Colors.grey,
+                                          )
+                                      ),
+                                      Text(
+                                        "공지사항",
+                                        style: TextStyle(
+                                            fontSize: 12,
+                                            color: Colors.grey
+                                        ),
+                                      )
+                                    ],
+                                  ),
+                                ),
+                              ),
+                              Container( // 메뉴: 안내 > 자주하는 질문
+                                child: InkWell(
+                                  onTap: () {
+                                    print("자주하는 질문 페이지로 이동");
+                                  },
+                                  child: Column(
+                                    children: [
+                                      Container(
+                                          margin: EdgeInsets.all(6),
+                                          child: Icon(
+                                            Icons.question_mark_sharp,
+                                            color: Colors.grey,
+                                          )
+                                      ),
+                                      Text(
+                                        "자주하는 질문",
+                                        style: TextStyle(
+                                            fontSize: 12,
+                                            color: Colors.grey
+                                        ),
+                                      )
+                                    ],
+                                  ),
+                                ),
+                              ),
+                              Container( // 메뉴: 안내 > 1:1 문의
+                                child: InkWell(
+                                  onTap: () {
+                                    print("1:1 문의 페이지로 이동");
+                                  },
+                                  child: Column(
+                                    children: [
+                                      Container(
+                                          margin: EdgeInsets.all(6),
+                                          child: Icon(
+                                            Icons.question_answer_sharp,
+                                            color: Colors.grey,
+                                          )
+                                      ),
+                                      Text(
+                                        "1:1 문의",
+                                        style: TextStyle(
+                                            fontSize: 12,
+                                            color: Colors.grey
+                                        ),
+                                      )
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  ],
+                ),
+                Column( // 하단
+                  children: [
+                    Container(
+                      padding: EdgeInsets.fromLTRB(0, 24, 0, 24),
+                      child: Row( // 이용약관, 설정, 로그아웃
                         mainAxisAlignment: MainAxisAlignment.spaceAround,
                         children: [
-                          Container( // 메뉴: 안내 > 연말정산
-                            child: InkWell(
-                              onTap: () {
-                                print("연말정산 페이지로 이동");
-                              },
-                              child: Column(
-                                children: [
-                                  Container(
-                                      margin: EdgeInsets.all(6),
-                                      child: Icon(
-                                        Icons.airplane_ticket_sharp,
-                                        color: Colors.grey,
-                                      )
-                                  ),
-                                  Text(
-                                    "연말정산",
-                                    style: TextStyle(
-                                        fontSize: 12,
-                                        color: Colors.grey // -> 연말정산 업데이트 되었을 때는, ttt 테마 색상으로 깜빡이게 하면 어떨까?
-                                    ),
-                                  )
-                                ],
-                              ),
-                            ),
+                          Container(
+                              child: InkWell(
+                                onTap: () {
+                                  print("이용약관 페이지로 이동");
+                                },
+                                child: Text("이용약관"),
+                              )
                           ),
-                          Container( // 메뉴: 안내 > 공지사항
-                            child: InkWell(
-                              onTap: () {
-                                print("공지사항 페이지로 이동");
-                              },
-                              child: Column(
-                                children: [
-                                  Container(
-                                      margin: EdgeInsets.all(6),
-                                      child: Icon(
-                                        Icons.announcement_sharp,
-                                        color: Colors.grey,
-                                      )
-                                  ),
-                                  Text(
-                                    "공지사항",
-                                    style: TextStyle(
-                                        fontSize: 12,
-                                        color: Colors.grey
-                                    ),
-                                  )
-                                ],
-                              ),
-                            ),
+                          Text("|", style: TextStyle(color: Colors.black12),),
+                          Container(
+                              child: InkWell(
+                                onTap: () {
+                                  print("설정 페이지로 이동");
+                                },
+                                child: Text("설정"),
+                              )
                           ),
-                          Container( // 메뉴: 안내 > 자주하는 질문
-                            child: InkWell(
-                              onTap: () {
-                                print("자주하는 질문 페이지로 이동");
-                              },
-                              child: Column(
-                                children: [
-                                  Container(
-                                      margin: EdgeInsets.all(6),
-                                      child: Icon(
-                                        Icons.question_mark_sharp,
-                                        color: Colors.grey,
-                                      )
-                                  ),
-                                  Text(
-                                    "자주하는 질문",
-                                    style: TextStyle(
-                                        fontSize: 12,
-                                        color: Colors.grey
-                                    ),
-                                  )
-                                ],
-                              ),
-                            ),
-                          ),
-                          Container( // 메뉴: 안내 > 1:1 문의
-                            child: InkWell(
-                              onTap: () {
-                                print("1:1 문의 페이지로 이동");
-                              },
-                              child: Column(
-                                children: [
-                                  Container(
-                                      margin: EdgeInsets.all(6),
-                                      child: Icon(
-                                        Icons.question_answer_sharp,
-                                        color: Colors.grey,
-                                      )
-                                  ),
-                                  Text(
-                                    "1:1 문의",
-                                    style: TextStyle(
-                                        fontSize: 12,
-                                        color: Colors.grey
-                                    ),
-                                  )
-                                ],
-                              ),
-                            ),
+                          Text("|", style: TextStyle(color: Colors.black12),),
+                          Container(
+                              child: InkWell(
+                                onTap: () {
+                                  print("로그아웃 되기");
+                                },
+                                child: Text("로그아웃"),
+                              )
                           ),
                         ],
                       ),
-                    )
+                    ),
+                    
                   ],
-                ),
-                
+                )
               ],
             )
           ),
